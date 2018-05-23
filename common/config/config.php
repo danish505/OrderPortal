3cea6f440566c17df8d93885d18336d5fac4fb08
Shahzad Fateh Ali <shahzad.fatehali@venturedive.com>
2018-05-23 13:27:37 +0500

    Removed unwanted files US Patient moved to Global Patient

--- common/config/config.php
+++ common/config/config.php
@@ -6,4 +6,4 @@ Define URLs here which will be accessible in all sub-applications
 */
 $config['public_url'] = 'http://global-patienttransfer.local';
 $config['admin_url'] = 'http://bo.global-patienttransfer.local';
-$config['site_title'] = 'US Patients Transfer';
+$config['site_title'] = 'Global Patient Transfer';
--- public_html/core/Public_Controller.php
+++ public_html/core/Public_Controller.php
@@ -1,25 +1,24 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-require_once APPPATH. '../common/core/USPT_Controller.php';
+require_once APPPATH. '../common/core/GPT_Controller.php';
 
-class Public_Controller extends USPT_Controller
+class Public_Controller extends GPT_Controller
 {
-	function __construct()
-	{
+    public function __construct()
+    {
+        parent::__construct();
 
-		parent::__construct();
-
-		// $maintenance_mode = $this->setting_model->get('maintenance_mode');
-		// if(!$maintenance_mode) {
-		// 	if($this->session->userdata('user_logged_in')) {
-		//
-		// 		$this->user = $user;
-		// 	} else {
-		// 		$this->isloggedin = FALSE;
-		// 	}
-		// } else {
-		// 	redirect('maintenance');
-		// }
-	}
+        // $maintenance_mode = $this->setting_model->get('maintenance_mode');
+        // if(!$maintenance_mode) {
+        // 	if($this->session->userdata('user_logged_in')) {
+        //
+        // 		$this->user = $user;
+        // 	} else {
+        // 		$this->isloggedin = FALSE;
+        // 	}
+        // } else {
+        // 	redirect('maintenance');
+        // }
+    }
 }
