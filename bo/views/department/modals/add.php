029355be14697802c0d75f2b9644c5b26f01bb88
Shahzad Fateh Ali <shahzad.fatehali@venturedive.com>
2018-07-06 13:36:16 +0500

    Departments

---
+++ bo/views/department/modals/add.php
@@ -0,0 +1,29 @@
+<div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addLabel">Add New Department</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <?php echo form_open(''); ?>
+                <div class="alert alert-danger d-none" role="alert">An error occurred while processing your request. Please try again.</div>
+                <input type="hidden" name="action" value="department_add" />
+                <div class="form-group row">
+                    <label for="department_name" class="col-sm-4 col-form-label">Name</label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="department_name" name="department_name" value="" required>
+                        <div class="invalid-feedback">Department Name is required.</div>
+                    </div>
+                </div>
+                <?php echo form_close(); ?>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" id="btn_department_add">Add Department</button>
+            </div>
+        </div>
+    </div>
+</div>
