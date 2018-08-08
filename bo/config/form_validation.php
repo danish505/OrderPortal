da0a76d21035974c2a86c76b1006085fc56808ba
Shahzad Fateh Ali <shahzad.fatehali@venturedive.com>
2018-08-08 17:56:05 +0500

    Admin ability to edit

--- bo/config/form_validation.php
+++ bo/config/form_validation.php
@@ -12,5 +12,35 @@ $config = array(
         'label' => 'Password',
         'rules' => 'required'
       ),
-  )
+    ),
+    'patient_profile' => array(
+      array(
+        'field' => 'first_name',
+        'label' => 'First name',
+        'rules' => 'required',
+        'error_prefix' => '<small id="error_first_name" class="form-text text-muted">',
+        'error_suffix' => '</small>'
+      ),
+      array(
+        'field' => 'last_name',
+        'label' => 'Last name',
+        'rules' => 'required',
+        'error_prefix' => '<small id="error_last_name" class="form-text text-muted">',
+        'error_suffix' => '</small>'
+      ),
+      array(
+        'field' => 'email_address',
+        'label' => 'Email Address',
+        'rules' => 'trim|required|valid_email',
+        'error_prefix' => '<small id="error_email_address" class="form-text text-muted">',
+        'error_suffix' => '</small>'
+      ),
+      array(
+        'field' => 'age',
+        'label' => 'Age',
+        'rules' => 'required',
+        'error_prefix' => '<small id="error_age_name" class="form-text text-muted">',
+        'error_suffix' => '</small>'
+      )
+    )
 );
